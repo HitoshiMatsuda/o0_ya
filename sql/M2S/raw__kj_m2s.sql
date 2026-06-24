322394f00ad71a1670e7c7a708e50754b8fa8c7c
hitoshi32 <programmer060732@gmail.com>
2026-06-24 22:23:15 +0900

    省略されていたクエリを修正した

--- sql/M2S/raw__kj_m2s.sql
+++ sql/M2S/raw__kj_m2s.sql
@@ -23,6 +23,7 @@
 -- ※上記条件に1つでも該当すると除外（not exists + or 条件）
 -- =====================================
 
+
 select
   -- ロール
   'initial_role' as role_id
@@ -66,7 +67,7 @@ select
 
   -- 郵便番号
   , if(item_004 = '', '', if(item_004 = ' ', '',
-      concat(substr(item_004, 1, 3), '-', substr(item_004, 4, 4))))
+      concat(substr(item_004,1,3),'-',substr(item_004,4,4))))
     as item_004
 
   -- 補足住所（漢字）
@@ -79,70 +80,97 @@ select
   , if(item_007 = ' ', '', replaceAll(item_007, '-', '')) as item_007
 
   -- 見込地区区分
-  , transform(ifNull(toString(item_024), ''), ['1','2','3'], ['関東','関西','名古屋'], '')
-    as item_024
+  , transform(ifNull(toString(item_024),''),['1','2','3'],['関東','関西','名古屋'],'') as item_024
 
   -- DM発行区分【お墓】
-  , transform(ifNull(toString(item_230), ''), ['1','2','3'], ['無','有','転居（住所不明）'], '')
-    as item_230
+  , transform(ifNull(toString(item_230),''),['1','2','3'],['無','有','転居（住所不明）'],'') as item_230
 
   -- 見込客初回登録日
   , if(length(item_026)=8,
       concat(substring(item_026,1,4),'/',substring(item_026,5,2),'/',substring(item_026,7,2)),
-      '')
-    as item_026
+      '') as item_026
 
   -- 紹介媒体区分（紹介媒体）
   , transform(
-      ifNull(toString(item_030), ''),
+      ifNull(toString(item_030),''),
       ['1','2','3','4','5','6','7','8','9','10',
        '11','12','13','14','15','16','17','18',
        '19','20','21','22','23','24','25','26',
        '27','28','29','30','31'],
-      [...元コードそのまま...],
+      [
+        'A011 当社客紹介','J040 個人開拓','A031 特約店紹介','B010 寺院紹介','B020 檀家紹介',
+        'A041 一般紹介対象','A042 一般紹介','A021 取引先紹介','A012 社友','A013 会友',
+        'A052 社員紹介','A053 大野屋グループOB','A061 紹介その他対象','A062 紹介その他',
+        'C010 葬儀社','C099 同業者その他','C020 石材店','C030 仏具店',
+        'A071 葬儀葬祭部門紹介','A072 仏壇仏具部門紹介',
+        'D010 互助会','D099 互助会その他',
+        'E010 職域団体','E020 病院','E099 職域団体その他',
+        'F010 立川髙島屋','F020 西友','F030 船橋東武','F040 東武外商部','F050 東武ギフトサロン','F099 デパートその他'
+      ],
       ''
     ) as item_030
 
   -- 紹介媒体区分（広告媒体）
   , transform(
-      ifNull(toString(item_031), ''),
-      arrayMap(x -> toString(x), range(1, 78)),
-      [...元コードそのまま...],
+      ifNull(toString(item_031),''),
+      arrayMap(x -> toString(x), range(1,78)),
+      [
+        'G010 TV','G020 ラジオ','G091 インターネット情報','G092 NTTドコモ',
+        'G03A 朝日新聞','G03B 毎日新聞','G03C 産経新聞','G03D 日経新聞','G03E 読売新聞',
+        'G03F 神戸新聞','G03G 京都新聞','G03H 中日新聞','G03I 東京新聞',
+        'G03J はいからエスト','G03K 定年時代','G03L タウン誌その他',
+        'G030 新聞','G037 インターカラー','G038 自治体おくやみハンドブック','G039 新聞その他',
+        'G040 ちらし','G060 電話帳広告','G031 タウン紙',
+        'G081 雑誌','G082 郵便局現金封筒','G083 はいから',
+        'I011 ポスティング（投げ込み）','I012 ポスティング（外部委託分）',
+        'H010 西武　交通','H020 小田急　交通','H030 京王　交通','H040 東武　交通','H050 地下鉄　交通','H099 交通その他',
+        'G050 看板','I010 一般Dメール','G080 通販',
+        'J020 テレホンアポイント','I020 買取','I021 ベルシステム24','I022 オートコール',
+        'G070 公営霊園イベント','G090 広報企画情報','G099 報道その他',
+        'G061 FAX情報BOX','G062 情報案内ダイヤル',
+        'J010 ひろい','J030 割当・当番',
+        'J050 ギャラリー千葉来店客','J051 ギャラリー横浜来店客','J052 都心営来店客','J053 福生展示場来店客',
+        'J054 千葉工事課来店客','J055 町田営業所来店客','J056 北関東支店来店客','J057 仏具北浦和来店客',
+        'J058 西関東支店来店客ギャラリーさぎ沼来店客','J059 来店客（関東地区）',
+        'J060 西関東支店　来店客','J061 多摩店来店客','J062 小平店来店客','J063 八王子営業所来店客',
+        'J070 綾瀬来店客','J080 千葉平和公園店来店客',
+        'G034 広報こうべ','G035 中日ショッパー','G036 広報名古屋',
+        'G100 セミナー・相談会（お墓関連）','G101 セミナー・相談会（葬儀関連）','G102 セミナー・相談会（名古屋）',
+        'I023 名簿','0159 オートテレコール','I110 ESM未入力','J099 紹介媒体その他（不明）',
+        'G032 リビング','G033 読売ファミリー','P010 メモリアルパートナー'
+      ],
       ''
     ) as item_031
 
   -- 見込みランク
-  , transform(ifNull(toString(item_034), ''), ['1','2','3','4','5','6','7','8','9'],
-    ['6ヶ月以内','6ヶ月以上','初期登録','不明','予備','即案内','名簿','没','失注'], '')
-    as item_034
+  , transform(ifNull(toString(item_034),''),['1','2','3','4','5','6','7','8','9'],
+    ['6ヶ月以内','6ヶ月以上','初期登録','不明','予備','即案内','名簿','没','失注'],'') as item_034
 
   -- 提携企業
   , item_040
 
   -- 墓地有無区分
-  , transform(ifNull(toString(item_035), ''), ['1','2'], ['無','有'], '')
-    as item_035
+  , transform(ifNull(toString(item_035),''),['1','2'],['無','有'],'') as item_035
 
   -- 顧客対象区分
-  , transform(ifNull(toString(item_036), ''), ['1','2'], ['非対象','対象'], '')
-    as item_036
+  , transform(ifNull(toString(item_036),''),['1','2'],['非対象','対象'],'') as item_036
 
   -- 性別
-  , transform(ifNull(toString(item_225), ''), ['1','2','3'], ['男性','女性','企業法人'], '')
-    as item_225
+  , transform(ifNull(toString(item_225),''),['1','2','3'],['男性','女性','企業法人'],'') as item_225
 
   -- 宗旨宗派
   , transform(
-      ifNull(toString(item_226), ''),
+      ifNull(toString(item_226),''),
       ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19'],
-      [...元コードそのまま...],
+      ['無宗派','神道','キリスト教','天台宗','真言宗','浄土宗','浄土真宗',
+       '浄土真宗本願寺（西本願寺）','真宗大谷派（東本願寺）','臨済宗','曹洞宗','日蓮宗',
+       '日蓮正宗','創価学会','立正佼正会','時宗','黄檗宗','禅宗','その他'],
       ''
     ) as item_226
 
   -- 情報区分
-  , transform(ifNull(toString(item_029), ''), ['1','2','3','4','5'],
-    ['寿陵','死亡','改葬','建替','不明'], '')
-    as item_029
+  , transform(ifNull(toString(item_029),''),['1','2','3','4','5'],
+    ['寿陵','死亡','改葬','建替','不明'],'') as item_029
 
   -- Eメールアドレス
   , item_010
@@ -150,17 +178,22 @@ select
   -- 生年月日
   , if(length(item_227)=8,
       concat(substring(item_227,1,4),'/',substring(item_227,5,2),'/',substring(item_227,7,2)),
-      '')
-    as item_227
+      '') as item_227
 
   -- 送付先　氏名
   , item_231
 
   -- 送付先　フリガナ
-  , arrayFold(...同様ロジック...) as item_232
+  , arrayFold(
+      (acc, t) -> replaceAll(acc, t.1, t.2),
+      [
+        ('ｶﾞ','ガ'),('ｷﾞ','ギ'),('ｸﾞ','グ'),('ｹﾞ','ゲ'),('ｺﾞ','ゴ')
+      ],
+      item_232
+    ) as item_232
 
   -- 送付先 郵便番号
-  , if(item_233 = '', '', if(item_233 = ' ', '',
+  , if(item_233='', '', if(item_233=' ', '',
       concat(substr(item_233,1,3),'-',substr(item_233,4,4))))
     as item_233
 
@@ -168,10 +201,9 @@ select
   , item_234
 
   -- 送付先 TEL
-  , if(item_235 = ' ', '', replaceAll(item_235, '-', ''))
-    as item_235
+  , if(item_235=' ','',replaceAll(item_235,'-','')) as item_235
 
-  -- 担当者　お墓〜サービス
+  -- 担当者
   , item_012
   , item_013
   , item_014
@@ -181,49 +213,43 @@ select
   , item_018
 
   -- 都営希望者区分
-  , transform(ifNull(toString(item_039), ''), ['1'], ['希望者'], '') as item_039
+  , transform(ifNull(toString(item_039),''),['1'],['希望者'],'') as item_039
 
   -- 外柵 有無区分
-  , transform(ifNull(toString(item_037), ''), ['1','2'], ['無','有'], '') as item_037
+  , transform(ifNull(toString(item_037),''),['1','2'],['無','有'],'') as item_037
 
   -- 石碑 有無区分
-  , transform(ifNull(toString(item_038), ''), ['1','2'], ['無','有'], '') as item_038
+  , transform(ifNull(toString(item_038),''),['1','2'],['無','有'],'') as item_038
 
   -- 顧客墓地状況区分
-  , transform(ifNull(toString(item_025), ''), ['1','2','3'],
-    ['見込客','申込客','決定客'], '') as item_025
+  , transform(ifNull(toString(item_025),''),['1','2','3'],
+    ['見込客','申込客','決定客'],'') as item_025
 
   -- 個人情報取得区分
-  , transform(ifNull(toString(item_237), ''), ['1','2','3'],
-    ['未確認','同意する','同意しない'], '') as item_237
+  , transform(ifNull(toString(item_237),''),['1','2','3'],
+    ['未確認','同意する','同意しない'],'') as item_237
 
   -- 確認日
   , if(length(item_238)=8,
       concat(substring(item_238,1,4),'/',substring(item_238,5,2),'/',substring(item_238,7,2)),
-      '')
-    as item_238
+      '') as item_238
 
   -- 担当者 確認
   , item_239
 
   -- 電話番号使用状況区分
-  , transform(ifNull(toString(item_009), ''), ['1','2','3'], ['不可','可','転居'], '')
-    as item_009
+  , transform(ifNull(toString(item_009),''),['1','2','3'],['不可','可','転居'],'') as item_009
 
   -- 送付先 電話番号使用状況区分
-  , transform(ifNull(toString(item_236), ''), ['1','2','3'], ['不可','可','転居'], '')
-    as item_236
+  , transform(ifNull(toString(item_236),''),['1','2','3'],['不可','可','転居'],'') as item_236
 
   -- Eメール送信有無区分
-  , transform(ifNull(toString(item_011), ''), ['1','2','3'], ['送信可','送信不可','未確認'], '')
-    as item_011
+  , transform(ifNull(toString(item_011),''),['1','2','3'],['送信可','送信不可','未確認'],'') as item_011
 
   -- 失注フラグ＿お墓
-  , transform(ifNull(toString(item_042), ''), ['1','2'], ['活動中','失注'], '')
-    as item_042
+  , transform(ifNull(toString(item_042),''),['1','2'],['活動中','失注'],'') as item_042
 
 from {selected_table} t
-
 where not exists (
   select 1
   from (
@@ -236,13 +262,12 @@ where not exists (
     and (
       (e.item_003 != '0' and e.cnt_003 > 1)
       or (e.item_001_tenki != '0' and e.cnt_001 > 1)
-      or (length(trim(e.item_004)) > 0 and length(replaceAll(e.item_004, '-', '')) != 7)
-      or (length(trim(e.item_233)) > 0 and length(replaceAll(e.item_233, '-', '')) != 7)
-      or (e.item_026 != '0' and length(replaceAll(e.item_026, '/', '')) != 8)
-      or (e.item_227 != '0' and length(replaceAll(e.item_227, '/', '')) != 8)
-      or (length(trim(e.phone_no)) > 0 and length(replaceAll(e.phone_no, '-', '')) not in (10, 11))
-      or (length(trim(e.item_007)) > 0 and length(replaceAll(e.item_007, '-', '')) not in (10, 11))
-      or (length(trim(e.item_235)) > 0 and length(replaceAll(e.item_235, '-', '')) not in (10, 11))
+      or (length(trim(e.item_004)) > 0 and length(replaceAll(e.item_004,'-','')) != 7)
+      or (length(trim(e.item_233)) > 0 and length(replaceAll(e.item_233,'-','')) != 7)
+      or (e.item_026 != '0' and length(replaceAll(e.item_026,'/','')) != 8)
+      or (e.item_227 != '0' and length(replaceAll(e.item_227,'/','')) != 8)
+      or (length(trim(e.phone_no)) > 0 and length(replaceAll(e.phone_no,'-','')) not in (10,11))
+      or (length(trim(e.item_007)) > 0 and length(replaceAll(e.item_007,'-','')) not in (10,11))
+      or (length(trim(e.item_235)) > 0 and length(replaceAll(e.item_235,'-','')) not in (10,11))
     )
-)
-;
+);
